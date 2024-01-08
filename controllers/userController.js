0d37ad5d790b5f6cb3d59ac94481a4bc8c8ad872
Joel Lopez <jolopez.satx@gmail.com>
2024-01-07 21:28:51 -0600

    finalized friends CRUD operations

--- controllers/userController.js
+++ controllers/userController.js
@@ -63,4 +63,43 @@ async updateUser(req, res) {
       res.status(500).json(err);
     }
   },
+
+  async addFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+      );
+  
+      if (!user) {
+        return res.status(404).json({ message: 'No User found with this ID!' });
+      }
+  
+      res.json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+  
+  async deleteFriend(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+  
+      if (!user) {
+        return res.status(404).json({ message: 'No User found with this ID!' });
+      }
+  
+      res.json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  }
+  
 };
